562269c36c1cf7a8e1bc81fafe79a715e7db82db
Tomas Barton <tomas.barton@stemcell.com>
2025-02-19 19:21:14 -0800

    add `gomod2nix` flake

---
+++ default.nix
@@ -0,0 +1,21 @@
+{ pkgs ? (
+    let
+      inherit (builtins) fetchTree fromJSON readFile;
+      inherit ((fromJSON (readFile ./flake.lock)).nodes) nixpkgs gomod2nix;
+    in
+    import (fetchTree nixpkgs.locked) {
+      overlays = [
+        (import "${fetchTree gomod2nix.locked}/overlay.nix")
+      ];
+    }
+  )
+, buildGoApplication ? pkgs.buildGoApplication
+}:
+
+buildGoApplication {
+  pname = "packer-plugin-arm-image";
+  version = "0.2.8";
+  pwd = ./.;
+  src = ./.;
+  modules = ./gomod2nix.toml;
+}
